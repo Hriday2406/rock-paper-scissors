6bf16e45dcd2d0b34fab9f5f7386056438456f57
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2024-12-19 16:33:31 +0530

    Add scoreboard

--- script.js
+++ script.js
@@ -1,90 +1,79 @@
 function getComputerChoice() {
-    const choice = [ "rock", "paper", "scissors" ];
-    return choice[ Math.floor(Math.random() * 3) ];
+  const choice = ["rock", "paper", "scissors"];
+  return choice[Math.floor(Math.random() * 3)];
 }
 
-function calcOutcomeText(outcome, choice1="", choice2=""){
-    if(outcome == "Tie")
-        return `It's a Tie.`;
-    return `You ${outcome}! ${choice1} beats ${choice2}.`;
+function updateScoreBoard() {
+  pScore.textContent = playerScore;
+  cScore.textContent = computerScore;
+}
+
+function calcOutcomeText(outcome, choice1 = "", choice2 = "") {
+  if (outcome == "Tie") return `It's a Tie.`;
+  return `You ${outcome}! ${choice1} beats ${choice2}.`;
 }
 
 function calcOutcome(playerChoice, computerChoice) {
-    if(playerChoice == "rock" && computerChoice == "scissors")
-        return calcOutcomeText("Win", "Rock", "Scissors");
-    else if(playerChoice == "paper" && computerChoice == "rock")
-        return calcOutcomeText("Win", "Paper", "Rock");
-    else if(playerChoice == "scissors" && computerChoice == "paper")
-        return calcOutcomeText("Win", "Scissors", "Paper");
-    else if(playerChoice == "scissors" && computerChoice == "rock")
-        return calcOutcomeText("Lose", "Rock", "Scissors");
-    else if(playerChoice == "rock" && computerChoice == "paper")
-        return calcOutcomeText("Lose", "Paper", "Rock");
-    else if(playerChoice == "paper" && computerChoice == "scissors")
-        return calcOutcomeText("Lose", "Scissors", "Paper");
-    else if(playerChoice == computerChoice)
-        return calcOutcomeText("Tie");
-    else
-        return "Wrong Input!";
+  if (playerChoice == computerChoice) return calcOutcomeText("Tie");
+  else if (playerChoice == "rock" && computerChoice == "scissors")
+    return calcOutcomeText("Win", "Rock", "Scissors");
+  else if (playerChoice == "paper" && computerChoice == "rock")
+    return calcOutcomeText("Win", "Paper", "Rock");
+  else if (playerChoice == "scissors" && computerChoice == "paper")
+    return calcOutcomeText("Win", "Scissors", "Paper");
+  else if (playerChoice == "scissors" && computerChoice == "rock")
+    return calcOutcomeText("Lose", "Rock", "Scissors");
+  else if (playerChoice == "rock" && computerChoice == "paper")
+    return calcOutcomeText("Lose", "Paper", "Rock");
+  else if (playerChoice == "paper" && computerChoice == "scissors")
+    return calcOutcomeText("Lose", "Scissors", "Paper");
+  else return "Wrong Input!";
 }
 
-function playRound(playerChoice, computerChoice = getComputerChoice()){
-    let outcome = calcOutcome(playerChoice, computerChoice);
+function playRound(playerChoice, computerChoice = getComputerChoice()) {
+  let outcome = calcOutcome(playerChoice, computerChoice);
 
-    outcomeDiv.textContent = outcome;
-    container.appendChild(outcomeDiv);
+  outcomeDiv.textContent = outcome;
+  container.appendChild(outcomeDiv);
 
-    if(outcome[4] == "W")
-        playerScore++;
-    else if(outcome[4] == "L")
-        computerScore++;
+  if (outcome[4] == "W") playerScore++;
+  else if (outcome[4] == "L") computerScore++;
 
-    if(playerScore == 5)
-        winner("player");
-    else if(computerScore == 5)
-        winner("computer");
+  updateScoreBoard();
+
+  if (playerScore == 10) winner("player");
+  else if (computerScore == 10) winner("computer");
 }
 
-function winner(string){
-    btnR.disabled = true;
-    btnP.disabled = true;
-    btnS.disabled = true;
-    container.removeChild(outcomeDiv);
-    const para1 = document.createElement('p');
-    const para2 = document.createElement('p');
-    const para3 = document.createElement('p');
-    if(string == "player")
-        para1.textContent = "You Won!!";
-    else if(string == "computer")
-        para1.textContent = "You Lost!!";
-    para2.textContent = `Player = ${playerScore}`
-    para3.textContent = `Computer = ${computerScore}`
-    para2.classList.add('marginAbove');
-    resultDiv.appendChild(para1);
-    resultDiv.appendChild(para2);
-    resultDiv.appendChild(para3);
-    container.appendChild(resultDiv);
+function winner(string) {
+  btnR.disabled = true;
+  btnP.disabled = true;
+  btnS.disabled = true;
+  if (string == "player") outcomeDiv.textContent = "You Won!!";
+  else if (string == "computer") outcomeDiv.textContent = "You Lost!!";
 }
 
 /* --------------------------------------------------------------- */
 
-let playerScore = 0, computerScore = 0;
-const btnR = document.querySelector('#btnR');
-const btnP = document.querySelector('#btnP');
-const btnS = document.querySelector('#btnS');
+let playerScore = 0,
+  computerScore = 0;
+const btnR = document.querySelector("#btnR");
+const btnP = document.querySelector("#btnP");
+const btnS = document.querySelector("#btnS");
 
-btnR.addEventListener('click', () => playRound('rock'));
-btnP.addEventListener('click', () => playRound('paper'));
-btnS.addEventListener('click', () => playRound('scissors'));
+const pScore = document.getElementById("pScore");
+const cScore = document.getElementById("cScore");
 
-const container = document.querySelector('.container');
-const outcomeDiv = document.createElement('div');
-outcomeDiv.classList.toggle('box');
+btnR.addEventListener("click", () => playRound("rock"));
+btnP.addEventListener("click", () => playRound("paper"));
+btnS.addEventListener("click", () => playRound("scissors"));
 
-const resultDiv = document.createElement('div');
-resultDiv.classList.toggle('box');
+const container = document.querySelector(".container");
+const outcomeDiv = document.createElement("div");
+outcomeDiv.classList.toggle("box");
 
-
+const resultDiv = document.createElement("div");
+resultDiv.classList.toggle("box");
 
 // playRound();
 
